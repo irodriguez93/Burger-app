add81e9b87196ffa26cc3f0d497f8d0c29500720
irodriguez93 <ir346csus@gmail.com>
2021-01-17 21:22:43 -0600

    l183

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -80,7 +82,7 @@ class BurgerBuilder extends Component {
   puchaseCanceledHandler = () => {
     this.setState({ purchasing: false });
   };
-  
+
   puchaseContinueHandler = () => {
     this.setState({ loading: true });
     //alert("Order Sucessful");
@@ -149,4 +151,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
